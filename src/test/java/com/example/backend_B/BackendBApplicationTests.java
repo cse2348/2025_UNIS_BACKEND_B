44b78116c0d4d14d2bfefc6934dea870e14b35d6
seoyeon <2371013@ewhain.net>
2025-06-23 20:24:46 +0900

    first commit

---
+++ src/test/java/com/example/backend_B/BackendBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
