e84a641e7a476a4787defb57bdb8287be0610e0c
seoyeon <2371013@ewhain.net>
2025-07-04 22:02:23 +0900

    redis commit

--- src/main/java/com/example/backend_B/entity/Comment.java
+++ src/main/java/com/example/backend_B/entity/Comment.java
@@ -17,7 +17,7 @@ public class Comment {
     @Id //대표키 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 자동으로 1씩 증가
     private Long id;
-    @ManyToOne //Comment와  Article엔티티를 다대일 관계로 설정
+    @ManyToOne // Comment와  Article엔티티를 다대일 관계로 설정
     @JoinColumn(name="article_id") //외래키 생성, Article 엔티티의 기본키(id)와 매핑
     private Article article; //해당 댓글의 부모 게시물
     @Column //해당 필드를 테이블의 속성으로 매핑
